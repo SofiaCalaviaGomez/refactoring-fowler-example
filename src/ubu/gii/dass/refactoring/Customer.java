dc38a8240fd16d5d12b0b5c54a80803ac9f1e989
SofiaCalaviaGomez <129267309+SofiaCalaviaGomez@users.noreply.github.com>
2025-05-15 19:36:35 +0200

    #3 moveMethod frequentRenterPoints

--- src/ubu/gii/dass/refactoring/Customer.java
+++ src/ubu/gii/dass/refactoring/Customer.java
@@ -42,7 +42,7 @@
 				thisAmount = each.getCharge();
 				
 				// add frequent renter points
-				frequentRenterPoints = getFrequentRenterPoints(frequentRenterPoints, each);
+				frequentRenterPoints = each.getFrequentRenterPoints(frequentRenterPoints);
 				// show figures for this rental
 				result += "\t" + each.getMovie().getTitle() + "\t"
 						+ String.valueOf(thisAmount) + "\n";
@@ -54,14 +54,5 @@
 					+ " frequent renter points";
 			return result;
 		}	
-
-		private int getFrequentRenterPoints(int frequentRenterPoints, Rental each) {
-		frequentRenterPoints++;
-		// add bonus for a two day new release rental
-		if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
-				&& each.getDaysRented() > 1)
-			frequentRenterPoints++;
-		return frequentRenterPoints;
-	}
 	
 	}
