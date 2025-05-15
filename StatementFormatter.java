35de31e5a96cf27dbf4b81925ab5d18fe015aae3
SofiaCalaviaGomez <129267309+SofiaCalaviaGomez@users.noreply.github.com>
2025-05-15 19:05:40 +0200

    Delete StatementFormatter.java

--- StatementFormatter.java
+++
@@ -1,8 +0,0 @@
-package ubu.gii.dass.refactoring;
-
-	interface StatementFormatter {
-        String header(String customerName);
-        String lineItem(String movieTitle, double amount);
-        String footer(double totalAmount, int frequentRenterPoints);
-    }
-
