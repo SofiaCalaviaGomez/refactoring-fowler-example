994719b5cbdfaa3c8ed6699a7fc9ff935a75e4e7
haleks5 <150005518+haleks5@users.noreply.github.com>
2025-05-15 19:19:21 +0200

    Add files via upload

--- src/ubu/gii/dass/refactoring/Customer.java
+++ src/ubu/gii/dass/refactoring/Customer.java
@@ -12,44 +12,50 @@
 	*/
 	import java.util.*;
 	
-	public class Customer {private String _name;
-    private List<Rental> _rentals = new ArrayList<>();
-
-    public Customer(String name) {
-        _name = name;
-    }
-
-    public void addRental(Rental arg) {
-        _rentals.add(arg);
-    }
-
-    public String getName() {
-        return _name;
-    }
-
-    public String htmlStatement() {
-        return generateStatement(new HtmlStatementFormatter());
-    }
-
-    public String statement() {
-        return generateStatement(new TextStatementFormatter());
-    }
-
-    private String generateStatement(StatementFormatter formatter) {
-        double totalAmount = 0;
-        int frequentRenterPoints = 0;
-        Iterator<Rental> rentals = _rentals.iterator();
-        StringBuilder result = new StringBuilder(formatter.header(getName()));
-
-        while (rentals.hasNext()) {
-            Rental each = rentals.next();
-            double thisAmount = each.getCharge();
-            frequentRenterPoints += each.getFrequentRenterPoints();
-            result.append(formatter.lineItem(each.getMovie().getTitle(), thisAmount));
-            totalAmount += thisAmount;
-        }
-
-        result.append(formatter.footer(totalAmount, frequentRenterPoints));
-        return result.toString();
-    }
-}
+	public class Customer {
+		private String _name;
+		private List<Rental> _rentals;
+	
+		public Customer(String name) {
+			_name = name;
+			_rentals = new ArrayList<Rental>();
+	
+		};
+	
+		public void addRental(Rental arg) {
+			_rentals.add(arg);
+		}
+	
+		public String getName() {
+			return _name;
+		};
+	
+		public String statement() {
+			double totalAmount = 0;
+			int frequentRenterPoints = 0;
+			Iterator<Rental> rentals = _rentals.iterator();
+			String result = "Rental Record for " + getName() + "\n";
+			while (rentals.hasNext()) {
+				double thisAmount = 0;
+				Rental each = rentals.next();
+				// determine amounts for each line
+				thisAmount = each.extracted(thisAmount);
+				
+				// add frequent renter points
+				frequentRenterPoints++;
+				// add bonus for a two day new release rental
+				if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
+						&& each.getDaysRented() > 1)
+					frequentRenterPoints++;
+				// show figures for this rental
+				result += "\t" + each.getMovie().getTitle() + "\t"
+						+ String.valueOf(thisAmount) + "\n";
+				totalAmount += thisAmount;
+			}
+			// add footer lines
+			result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+			result += "You earned " + String.valueOf(frequentRenterPoints)
+					+ " frequent renter points";
+			return result;
+		}	
+	}
